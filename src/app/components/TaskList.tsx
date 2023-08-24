62072d55ee7d3de0467f8a18611aa495aa926854
Amrit Thapa <amritthapa966@gmail.com>
2023-08-24 20:25:20 +0530

    breaking component

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -1,120 +1,121 @@
-import React, {ComponentProps, SyntheticEvent, useState} from "react";
-import {flushSync} from "react-dom";
+import React, {ComponentProps, useState} from "react";
+import {twMerge} from "tailwind-merge";
 import {Task, TaskType, useAppContext} from "../AppContextProvider";
 
 type Props = {
   taskList: Task[];
   type: TaskType;
   setter: React.Dispatch<React.SetStateAction<Task[]>>;
-};
+} & ComponentProps<"div">;
 
-const Label = ({children}: ComponentProps<"div">) => {
+const Label = ({children, className}: ComponentProps<"div">) => {
   return (
     <div>
-      <label className={"p-1 bg-green-200 rounded shadow-sm"}>{children}</label>
+      <label
+        className={twMerge("p-1 bg-green-200 rounded shadow-sm", className)}
+      >
+        {children}
+      </label>
     </div>
   );
 };
 
-const TaskList = ({
-  taskList,
-  setter,
-  type,
-  onDrop,
-  children,
-}: ComponentProps<"div"> & Props) => {
-  const {setSelectedTask} = useAppContext();
+const ListItem = ({taskList, onDrop, setter, type}: Props) => {
+  const {selectedTask, setSelectedTask} = useAppContext();
   const [openForm, updateOpenForm] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
   const handleTaskSubmit = (task: Task) => {
     setter((prev) => [task, ...prev]);
-    setDescription("");
     setTitle("");
     updateOpenForm(false);
   };
 
   return (
     <>
-      <div className="flex flex-col h-full gap-2">
-        {children}
-        {!!taskList.length &&
-          taskList.map((task) => {
-            return (
-              <div
-                key={task.id}
-                className="p-2 border rounded shadow-sm hover:bg-gray-100"
-                draggable
-                onDragOver={(event) => {
-                  event.stopPropagation();
-                  event.preventDefault();
-                }}
-                onDragStart={() => setSelectedTask(task)}
-                onDrop={onDrop}
-              >
-                {task.title}
-              </div>
-            );
-          })}
-        <div
-          onClick={() => updateOpenForm(true)}
-          className="p-2 text-gray-500 rounded cursor-pointer hover:bg-gray-100"
-          onDrop={onDrop}
-          onDragOver={(event) => {
-            event.stopPropagation();
-            event.preventDefault();
-          }}
-        >
-          + New
-        </div>
-      </div>
+      {!!taskList.length &&
+        taskList.map((task) => {
+          return (
+            <div
+              key={task.id}
+              className="border rounded shadow-sm hover:bg-gray-100"
+              draggable
+              onDragOver={(event) => {
+                event.preventDefault();
+                if (task.id !== selectedTask?.task.id) {
+                  // event.target.style.borderTop = "thick solid #77AAFF";
+                }
+              }}
+              onDragEnd={(event) => {
+                event.preventDefault();
+                // event.target.style.borderTop = "none";
+              }}
+              onDragStart={() => setSelectedTask({task, sourceUpdater: setter})}
+              onDrop={() => onDrop}
+            >
+              {task.title}
+            </div>
+          );
+        })}
+
       {openForm && (
-        <div
-          className="absolute top-0 left-0 w-screen h-screen bg-gray-100 bg-opacity-60"
-          onClick={() => updateOpenForm(false)}
-        >
-          <div
-            className="absolute z-10 flex flex-col justify-center h-64 gap-4 p-5 m-auto -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-lg cursor-pointer top-1/2 left-1/2 w-80"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div className="flex items-end justify-between">
-              <label>Title:</label>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="border-b-2"
-              />
-            </div>
-            <div className="flex items-end justify-between">
-              <label>Description:</label>
-              <input
-                type="text"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="border-b-2"
-              />
-            </div>
-            <button
-              onClick={() =>
+        <div className="border rounded shadow-sm hover:bg-gray-100">
+          <input
+            type="text"
+            className="w-full h-full p-2 border rounded focus:outline-none"
+            value={title}
+            onKeyUp={(e) => {
+              if (e.code === "Enter") {
                 handleTaskSubmit({
-                  title,
-                  id: title.replaceAll(" ", "_").toLowerCase(),
+                  title: title || "Untitled",
+                  id: Date.now(),
+                  index: taskList.length + 1,
                   description,
                   status: type,
-                })
+                });
               }
-            >
-              submit
-            </button>
-          </div>
+            }}
+            autoFocus
+            onBlur={() => {
+              handleTaskSubmit({
+                title: title || "Untitled",
+                id: Date.now(),
+                index: taskList.length + 1,
+                description,
+                status: type,
+              });
+            }}
+            placeholder="Type name..."
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
       )}
+      <div
+        onClick={() => updateOpenForm(true)}
+        className="p-2 text-gray-500 rounded cursor-pointer hover:bg-gray-100"
+        onDrop={onDrop}
+        onDragEnd={(event) => {
+          event.preventDefault();
+          // event.target.style.borderTop = "none";
+        }}
+        onDragOver={(event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          // event.target.style.borderTop = "2px solid #95DCFF";
+        }}
+      >
+        +New
+      </div>
     </>
   );
 };
 
+const TaskList = ({children}: ComponentProps<"div">) => {
+  return <div className="flex flex-col h-full gap-2">{children}</div>;
+};
+
 TaskList.Label = Label;
+TaskList.ListItem = ListItem;
 
 export default TaskList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import {useEffect} from "react";
 import {TaskType, useAppContext} from "./AppContextProvider";
 import TaskList from "./components/TaskList";
 
@@ -17,36 +16,37 @@ export default function Home() {
     setSelectedTask,
   } = useAppContext();
 
-  useEffect(() => {
-    console.log(selectedTask);
-  }, [selectedTask]);
-
   const handleTaskDrop = (destination: TaskType) => {
     if (!selectedTask) return;
-    if (selectedTask.status === destination) return;
+    if (selectedTask.task.status === destination) return;
+
+    selectedTask.sourceUpdater((prev) => {
+      const temp = prev.filter((item) => item.id !== selectedTask.task.id);
+      return temp;
+    });
 
     switch (destination) {
       case TaskType.backlog:
         setBacklogTask((prev) => [
-          {...selectedTask, status: TaskType.backlog},
+          {...selectedTask.task, status: TaskType.backlog},
           ...prev,
         ]);
         break;
       case TaskType.created:
         setCreatedTask((prev) => [
-          {...selectedTask, status: TaskType.created},
+          {...selectedTask.task, status: TaskType.created},
           ...prev,
         ]);
         break;
       case TaskType.done:
         setDoneTask((prev) => [
-          {...selectedTask, status: TaskType.done},
+          {...selectedTask.task, status: TaskType.done},
           ...prev,
         ]);
         break;
-      case TaskType["in-progress"]:
+      case TaskType.inProgress:
         setInProgressTask((prev) => [
-          {...selectedTask, status: TaskType["in-progress"]},
+          {...selectedTask.task, status: TaskType["inProgress"]},
           ...prev,
         ]);
         break;
@@ -56,41 +56,54 @@ export default function Home() {
 
   return (
     <div className="grid grid-cols-5 min-h-[calc(100vh-114px)] gap-3 p-2">
-      <TaskList
-        taskList={createdTask}
-        setter={setCreatedTask}
-        type={TaskType.created}
-        onDrop={() => handleTaskDrop(TaskType.created)}
-      >
-        <TaskList.Label>Task</TaskList.Label>
+      <TaskList>
+        <div className="flex gap-2">
+          <TaskList.Label>Task</TaskList.Label>
+          <span>{createdTask.length}</span>
+        </div>
+        <TaskList.ListItem
+          setter={setCreatedTask}
+          type={TaskType.created}
+          taskList={createdTask}
+          onDrop={() => handleTaskDrop(TaskType.created)}
+        />
       </TaskList>
-      <TaskList
-        taskList={backlogTask}
-        setter={setBacklogTask}
-        type={TaskType.backlog}
-        onDrop={() => handleTaskDrop(TaskType.backlog)}
-      >
-        <TaskList.Label>BackLog</TaskList.Label>
+      <TaskList>
+        <div className="flex gap-2">
+          <TaskList.Label className="bg-red-200">BackLog</TaskList.Label>
+          <span>{backlogTask.length}</span>
+        </div>
+        <TaskList.ListItem
+          setter={setBacklogTask}
+          type={TaskType.backlog}
+          taskList={backlogTask}
+          onDrop={() => handleTaskDrop(TaskType.backlog)}
+        />
       </TaskList>
-      <TaskList
-        taskList={inProgressTask}
-        setter={setInProgressTask}
-        type={TaskType["in-progress"]}
-        onDrop={() => handleTaskDrop(TaskType["in-progress"])}
-      >
-        <TaskList.Label>In Progress</TaskList.Label>
+      <TaskList>
+        <div className="flex gap-2">
+          <TaskList.Label className="bg-blue-200">In Progress</TaskList.Label>
+          <span> {inProgressTask.length}</span>
+        </div>
+        <TaskList.ListItem
+          setter={setInProgressTask}
+          type={TaskType.inProgress}
+          taskList={inProgressTask}
+          onDrop={() => handleTaskDrop(TaskType.inProgress)}
+        />
       </TaskList>
-      <TaskList
-        taskList={doneTask}
-        setter={setDoneTask}
-        type={TaskType.done}
-        onDrop={() => handleTaskDrop(TaskType.done)}
-      >
-        <TaskList.Label>Done</TaskList.Label>
+      <TaskList>
+        <div className="flex gap-2">
+          <TaskList.Label className="bg-purple-200">Done</TaskList.Label>
+          <span>{doneTask.length}</span>
+        </div>
+        <TaskList.ListItem
+          setter={setDoneTask}
+          type={TaskType.done}
+          taskList={doneTask}
+          onDrop={() => handleTaskDrop(TaskType.done)}
+        />
       </TaskList>
-      {/* <TaskList taskList={doneTask} setter={setDoneTask}>
-        <TaskList.Label>Done</TaskList.Label>
-      </TaskList> */}
     </div>
   );
 }
