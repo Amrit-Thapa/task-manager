d36a1811cae0423d9d09e33ba7863250e1ad7e61
Amrit Thapa <amritthapa966@gmail.com>
2023-10-04 10:10:19 +0530

    adding edit button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type {Metadata} from "next";
 import {ComponentProps} from "react";
+import {MdOutlineAddTask} from "react-icons/md";
 import {AppContextProvider} from "./AppContextProvider";
 
 export const metadata: Metadata = {
@@ -19,8 +20,21 @@ export default function RootLayout({children}: ComponentProps<"body">) {
           className="flex max-w-[1600px] m-auto"
           suppressHydrationWarning={true}
         >
-          <div className="w-full px-4">
-            <div className="flex items-end w-full h-32" id="title-portal"></div>
+          <div className="w-full p-4">
+            <div className="flex flex-col items-start gap-5">
+              <div className="flex items-center justify-center gap-2 text-4xl">
+                <MdOutlineAddTask />
+                Plan Your Tasks
+              </div>
+              <div>
+                <li>
+                  Click + New to create a new task directly on this board.{" "}
+                </li>
+                <li>
+                  Click an existing task to add additional context or subtasks.
+                </li>
+              </div>
+            </div>
             <div className="pt-10">{children}</div>
           </div>
         </body>
